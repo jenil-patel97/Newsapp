47933857d23172b9f2f93ea7a4ba9fca5c8ed33a
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-07-23 19:06:29 +0530

    Add: imageUrl

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
           <img
-            src="https://cdn.24.co.za/files/Cms/General/d/3288/82949ce414a84ddc85b9723494ee4816.jpg"
+            src={imageUrl}
             className="card-img-top"
             alt="..."
           />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">
-              Go somewhere
+            <a href="/newsdetail" className="btn btn-sm btn-primary">
+              Read More
             </a>
           </div>
         </div>
