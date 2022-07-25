f24533cba9f3939a51bcbba9e3604d6b71fc43f0
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-07-25 15:58:34 +0530

    Add: Top loading bar

--- src/components/News.js
+++ src/components/News.js
@@ -35,15 +35,19 @@ export class News extends Component {
   }
 
   async updateNews() {
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b202f6cd9b7646c5bbf5867a5c22df76&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
+    this.props.setProgress(70);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100);
   }
 
   async componentDidMount() {
@@ -75,7 +79,7 @@ export class News extends Component {
   render() {
     return (
       <>
-        <h1 className="text-center" style={{ margin: "35px, 0px" }}>
+        <h1 className="text-center my-2" style={{ margin: "35px, 0px" }}>
           NewsTak - Top {this.capitalizeFirstLetter(this.props.category)}{" "}
           Headlines
         </h1>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,12 +7,17 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card">
-          <span
-            className="position-absolute top-0 translate-middle badge rounded-pill bg-success"
-            style={{ left: "90%", zIndex: "1" }}
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+              position: "absolute",
+              right: "0",
+            }}
           >
-            {source}
-          </span>
+            <span className="badge rounded-pill bg-success">{source}</span>
+          </div>
+
           <img
             src={
               !imageUrl
