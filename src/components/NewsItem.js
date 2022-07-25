0c8726eeaa2410c15f8609e2ce400d9054fb98e5
Jenil Patel <98316039+jenil-patel97@users.noreply.github.com>
2022-07-25 11:20:13 +0530

    Added time, author & news source

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,19 +1,42 @@
 import React, { Component } from "react";
 
-// author, date, source 
-
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl} =  
+    let { title, description, imageUrl, newsUrl, author, date, source } =
       this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={!imageUrl?"https://images.hindustantimes.com/img/2022/07/24/1600x900/aaditya_thackeray_1658629218034_1658629257778_1658629257778.jpg":imageUrl} className="card-img-top" alt="..." />
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-success"
+            style={{ left: "90%", zIndex: "1" }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://images.hindustantimes.com/img/2022/07/24/1600x900/aaditya_thackeray_1658629218034_1658629257778_1658629257778.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
               Read More
             </a>
           </div>
